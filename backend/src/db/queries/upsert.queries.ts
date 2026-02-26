2b81c52ae28b94df1c43ddd48b9090612b8f2d4d
Renzify <168178279+Renzify@users.noreply.github.com>
2026-02-26 14:14:20 +0800

    add upsert queries

--- backend/src/db/queries/upsert.queries.ts
+++ backend/src/db/queries/upsert.queries.ts
@@ -1,11 +1,5 @@
 import { db } from "../index.ts";
 import {
-  event,
-  competition,
-  category,
-  choice,
-  judge,
-  contestant,
   type NewEvent,
   type NewCompetition,
   type NewCategory,
@@ -19,6 +13,7 @@ import {
   getCategoryById,
   getChoiceById,
   getJudgeById,
+  getContestantById,
 } from "./id-select-queries.ts";
 import {
   updateEvent,
@@ -26,6 +21,7 @@ import {
   updateCategory,
   updateChoice,
   updateJudge,
+  updateContestant,
 } from "./update-queries.ts";
 import {
   createEvent,
@@ -33,4 +29,47 @@ import {
   createCategory,
   createChoice,
   createJudge,
+  createContestant,
 } from "./create.queries.ts";
+
+export const upsertEvent = async (data: NewEvent) => {
+  const existingEvent = await getEventById(data.id);
+  if (existingEvent) return updateEvent(data.id, data);
+
+  return createEvent(data);
+};
+
+export const upsertCompetition = async (data: NewCompetition) => {
+  const existingCompetition = await getCompetitionById(data.id);
+  if (existingCompetition) return updateCompetition(data.id, data);
+
+  return createCompetition(data);
+};
+
+export const upsertCategory = async (data: NewCategory) => {
+  const existingCategory = await getCategoryById(data.id);
+  if (existingCategory) return updateCategory(data.id, data);
+
+  return createCategory(data);
+};
+
+export const upsertChoice = async (data: NewChoice) => {
+  const existingChoice = await getChoiceById(data.id);
+  if (existingChoice) return updateChoice(data.id, data);
+
+  return createChoice(data);
+};
+
+export const upsertJudge = async (data: NewJudge) => {
+  const existingJudge = await getJudgeById(data.id);
+  if (existingJudge) return updateJudge(data.id, data);
+
+  return createJudge(data);
+};
+
+export const upsertContestant = async (data: NewContestant) => {
+  const existingContestant = await getContestantById(data.id);
+  if (existingContestant) return updateContestant(data.id, data);
+
+  return createContestant(data);
+};
